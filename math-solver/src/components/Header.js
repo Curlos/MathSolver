f26ab9060e79c3f754135a20c38bbc9969782f1e
Carlos Martinez <curlosmart@gmail.com>
2021-01-03 22:01:35 -0500

    Initial frontend setup

---
+++ math-solver/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div>
+      Worksheets Header
+    </div>
+  )
+}
+
+export default Header;
---
+++ math-solver/src/router/AppRouter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MathWorksheetsList from '../components/MathWorksheetsList';
+import Header from '../components/Header';
+
+const AppRouter = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default AppRouter;
